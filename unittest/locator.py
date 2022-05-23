67ce9b62d612b11aec220025039c27414eff3916
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-05-23 22:36:32 +0700

    update

---
+++ unittest/locator.py
@@ -0,0 +1,8 @@
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+
+class MainPageLocators(object):
+    GO_BUTTON = (By.ID, "submit")
+
+class SearchResultsPageLocators(object):
+    pass
--- unittest/main.py
+++ unittest/main.py
@@ -5,13 +5,17 @@ import page
 
 class PythonOrgSearch(unittest.TestCase):
 
-    def setup(self):
+    def setUp(self):
+        print("setup")
         self.driver = webdriver.Chrome("C:\\Program Files (x86)\\chromedriver.exe")
-        self.driver.get("htttp://www.python.org")
+        self.driver.get("http://www.python.org")
 
-    def test_example(self):
+    def test_title(self):
+        mainPage = page.MainPage()
+        assert mainPage.is_title_matches()
         
-
-
     def tearDown(self):
         self.driver.close()
+
+if __name__ == "__main__" :
+    unittest.main()
