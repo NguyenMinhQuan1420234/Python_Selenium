3b3cb1061e1b14e87f493cc23c3b08bfef1593c0
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-05-26 13:22:29 +0700

    update

--- test.py
+++ test.py
@@ -1,3 +1,11 @@
-x, y = map(int, input("Enter multiple values: ").split())
-print(type(x),type(y))
-print(f"so x = {int(x)} , y = {int(y)}")
+# x, y = map(int, input("Enter multiple values: ").split())
+# print(type(x),type(y))
+# print(f"so x = {int(x)} , y = {int(y)}")
+# x , y = y, x
+# print(x, y)
+z = [[i*j for i in range(5)] for j in range (5)]
+print(z)
+word = " hello you are piece of shit "
+print(len(word))
+x = {char: word.count(char) for char in set(word)}
+print(x)
