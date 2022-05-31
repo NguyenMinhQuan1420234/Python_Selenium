31d9343d736461caae4a56e1813dd0f7b28c6aed
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-05-31 21:40:40 +0700

    update

--- upload_download/test.py
+++ upload_download/test.py
@@ -1,15 +1,16 @@
 import requests #
 
-
+# download to working folder
 
 downloadUrl = 'https://binaries.templates.cdn.office.net/support/templates/en-us/tf16402488_win32.dotx'
 
+output_dir = r'C:\Users\banch\Python_Selenium\upload_download'
 # req = requests.get(downloadUrl)
 
 # filename = req.url[downloadUrl.rfind('/')+1:]
 
 # with open(filename, 'wb') as f :
-#     for chunk in req.iter_content(chunk_size=8192):
+#     for chunk in req.iter_content(chunk_size=8192):   
 #         if chunk:
 #             f.write(chunk)
 
