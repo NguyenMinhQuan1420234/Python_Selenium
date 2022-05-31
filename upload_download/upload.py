80754e927b46d93a760840e35f4cf2818bf73110
Nguyễn Minh Quân <35888563+NguyenMinhQuan1420234@users.noreply.github.com>
2022-05-31 23:12:05 +0700

    update

--- upload_download/upload.py
+++ upload_download/upload.py
@@ -4,7 +4,7 @@ url = 'https://httpbin.org/post'
 
 files = {'file': open('USCarBrands.csv', 'rb')}
 
-r = request.post(url, files= files)
+r = requests.post(url, files= files)
 
 print(r.status_code)
 print(r.text)
